086381696f76cf208b98cdc5b251e7a3f787c51c
Elisa BOURG <zaza3355@hotmail.fr>
2022-11-17 13:30:45 +0100

    add footer

--- lebeaumonde/tailwind.config.cjs
+++ lebeaumonde/tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        "green-sapin": "#458966",
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                "green-sapin": "#458966",
+                "black-black": "#242424",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
